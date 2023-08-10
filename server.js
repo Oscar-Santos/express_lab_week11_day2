4715bfd8cbdbd5dd68bfdf1174c634b33dbd4b37
Oscar Santos <oscar303data@gmail.com>
2023-08-09 19:38:44 -0600

    99 bottles of beer app done

--- server.js
+++ server.js
@@ -7,9 +7,6 @@ const PORT = 3001
 const responses = require('./models/responses')
 
 // greetings
-app.get('/', (req, res) => {
-    res.send('Hello hello!')
-})
 
 app.get('/greeting', (req, res) => {
     res.send('Hello stranger!')
@@ -31,7 +28,19 @@ app.get('/magic/:question', (req, res) => {
     const random = Math.floor(Math.random() * responses.length)
     res.send(`<h1>${req.params.question}</h1> <br/> <h2>${responses[random]}</h2>`)
 })
+// 99 bottles of beer on the wall
 
+app.get('/', (req, res) => {
+    res.send('99 bottles of beer on the wall')
+})
+
+app.get('/:number_of_bottles', (req, res) => {
+    if (req.params.number_of_bottles > 0) {
+        res.send(`${req.params.number_of_bottles} bottles of beer on the wall <br/> <a href="/${req.params.number_of_bottles - 1}">Take one down, pass it around</a>`)
+    } else {
+        res.send(`<a href="/">Start Over</a>`)
+    }
+})
 
 
 
