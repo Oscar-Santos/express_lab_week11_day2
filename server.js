991ebc78aafb07f69bddbe252a42babd2415e9bf
Oscar Santos <oscar303data@gmail.com>
2023-08-09 18:44:24 -0600

    tip calculator express application created

--- server.js
+++ server.js
@@ -4,6 +4,7 @@ const app = express();
 
 const PORT = 3001
 
+// greetings
 app.get('/', (req, res) => {
     res.send('Hello hello!')
 })
@@ -15,6 +16,17 @@ app.get('/greeting', (req, res) => {
 app.get('/greeting/:name', (req, res) => {
     res.send(`Hello ${req.params.name}! is so great to see you!`)
 })
+
+// tip calculator
+
+app.get('/tip/:total/:tipPercentage', (req, res) => {
+    res.send(`Your tip is $${req.params.total * req.params.tipPercentage / 100}`)
+})
+
+
+
+
+
 
 app.listen(PORT, () => {
     console.log('Server listening on port 3001');
